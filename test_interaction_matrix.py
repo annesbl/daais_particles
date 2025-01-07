692e0eafd5a525b1f4187f770d17ea796a3c2e61
annesbl <annesoballa@icloud.com>
2025-01-07 17:29:00 +0100

    added last tests for matrix

--- test_interaction_matrix.py
+++ test_interaction_matrix.py
@@ -12,3 +12,13 @@ class TestInteractionMatrix(unittest.TestCase):
 
     def test_matrix_initialization(self):
         self.assertEqual(self.matrix.matrix.shape, (3, 3))
+        
+    def test_get_interaction(self):
+        interaction = self.matrix.get_interaction("A", "B")
+        self.assertTrue(-1 <= interaction <= 1)
+
+    def test_calculate_force(self):
+        particle1 = Particle(position=[0, 0], velocity=[0, 0], particle_type="A")
+        particle2 = Particle(position=[3, 4], velocity=[0, 0], particle_type="B")
+        force = self.matrix.calculate_force(particle1, particle2)
+        self.assertEqual(force.shape, (2,))
