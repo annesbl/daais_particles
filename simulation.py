e6daff58f59ba60eb2b7af69ad07fa2cc3354aae
annesbl <annesoballa@icloud.com>
2025-01-07 17:22:04 +0100

    added imports and class name

---
+++ simulation.py
@@ -0,0 +1,7 @@
+import pygame
+from ParticleSystem import ParticleSystem
+from Board import Board
+from InteractionMatrix import InteractionMatrix
+
+class Simulation:
+    def __init__(self, width, height, particle_count, particle_types):
