7234576d2e087b0f1ce697b6de7b259c3e42c693
julnas <jule.nassenstein@study.hs-duesseldorf.de>
2025-02-16 04:19:06 +0100

    changed the names of the imports in simulation

--- simulation.py
+++ simulation.py
@@ -1,7 +1,7 @@
 import pygame
-from ParticleSystem import ParticleSystem
-from Board import Board
-from InteractionMatrix import InteractionMatrix
+from particle_system import ParticleSystem
+from board import Board
+from matrix import InteractionMatrix
 
 class Simulation:
     def __init__(self, width, height, particle_count, particle_types):
